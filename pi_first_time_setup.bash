0437ac4ab3dbd9410165eab60407c1870daeac11
pusalieth <pusalieth@users.noreply.github.com>
2018-03-05 02:21:04 -0700

    Added password autoset

--- pi_first_time_setup.bash
+++ pi_first_time_setup.bash
@@ -2,6 +2,8 @@
 
 ## set Camera_Control.bash into root cron job and run in background
 sudo su;
+echo -n "echo -n '"'raspberry'"'";
+passwd;
 echo -n "echo -n '"'eaXQY7SVfXS583vkdSPfzGqT'"'";
 
 chmod +x 755 Camera_Control.sh;
